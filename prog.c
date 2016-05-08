5400f3956c87e9c572c58faf03facfec8ef59abb
Manu <biedsteak@hotmail.de>
2016-05-08 09:02:17 +0200

    c not cpp

--- prog.c
+++ prog.c
@@ -1,9 +1,14 @@
-#include <iostream>
+#include <stdio.h>
 
 #include <pigpio.h>
 
 int main(int argc, char *argv[])
 {
-  if (gpioInitialise() < 0) return 1;
-  else std::cout <<"gpio initialised..." << std::endl;
+  if (gpioInitialise() < 0){
+   printf("gpio failed to initialise, EXIT.\n");	
+   return 1;
+  }
+  else printf("gpio initialised.\n");
+
+  return 0;
 }
